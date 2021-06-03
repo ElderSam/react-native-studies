8af7101807534044b35f472d78c2be012879f194
ElderSam <eldersamuel98@gmail.com>
2021-06-03 15:36:25 -0300

    ECMAScript Module System

--- curso-react-native/exercicios/App.js
+++ curso-react-native/exercicios/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
 import Primeiro from './src/components/Primeiro';
+import CompX, { Comp1, Comp2 } from './src/components/Multi';
 
 export default function App() {
 
   return (
     <SafeAreaView>
-      <Text>My App</Text>
-      <Text>{1 + 1}</Text>
-
       <Primeiro/>
+
+      <CompX/>
+      <Comp1/>
+      <Comp2/>
     </SafeAreaView>
   );
 }
---
+++ curso-react-native/exercicios/src/components/Multi.js
@@ -0,0 +1,20 @@
+/* ECMAScript Module System
+multiple components in a single file
+*/
+
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Comp() {
+    return <Text>Comp #Oficial</Text>
+}
+
+function Comp1() {
+    return <Text>Comp #01</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #02</Text>
+}
+
+export { Comp1, Comp2 }
