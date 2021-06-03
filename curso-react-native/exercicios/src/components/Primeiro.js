0f57e2e59ef6b3515e50b43f0772f592951d89fe
ElderSam <eldersamuel98@gmail.com>
2021-06-03 16:05:55 -0300

    debug app using 'console.warn'

--- curso-react-native/exercicios/src/components/Primeiro.js
+++ curso-react-native/exercicios/src/components/Primeiro.js
@@ -3,4 +3,10 @@ import { Text } from 'react-native';
 
 import Style from './Style';
 
-export default () => <Text style={Style.bigText}>Primeiro Componente</Text>
+export default () => {
+    console.warn('first component') // to debug your app
+
+    return (
+        <Text style={Style.bigText}>Primeiro Componente</Text>
+    )
+}
