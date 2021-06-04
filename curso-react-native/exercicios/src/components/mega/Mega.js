f002697c70fb5f4724308510a18c5bdb2f181694
ElderSam <eldersamuel98@gmail.com>
2021-06-04 16:53:10 -0300

    Component 'Mega-Sena' (random number generator)

--- curso-react-native/exercicios/src/components/mega/Mega.js
+++ curso-react-native/exercicios/src/components/mega/Mega.js
@@ -1,16 +1,33 @@
+/* random number generator */
+
 import React, { Component } from 'react'
-import { Text, TextInput } from 'react-native'
+import { Text, TextInput, Button } from 'react-native'
 
 import Style from '../Style'
 
 export default class Mega extends Component {
 
     state = {
-        qtdNumeros: this.props.qtdNumeros
+        qtdNumeros: this.props.qtdNumeros,
+        numeros: []
     }
 
     changeQtdNumero = (qtde) => {
-        this.setState({ qtdNumeros: qtde })
+        this.setState({ qtdNumeros: +qtde })
+    }
+
+    gerarNumeroNaoContido = nums => {
+        const novo = parseInt(Math.random() * 60) + 1 // random number 1-60
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+    }
+
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtdNumeros)
+            .fill()
+            .reduce(n => [...n, this.gerarNumeroNaoContido(n)], [])
+            .sort((a, b) => a - b)
+
+        this.setState({ numeros })
     }
 
     render() {
@@ -18,16 +35,22 @@ export default class Mega extends Component {
             <>
                 <Text style={Style.bigText}>
                     Gerador de Mega-Sena 
-                    {this.state.qtdNumeros}
                 </Text>
 
                 <TextInput
                     keyboardType={'numeric'}
                     style={{borderBottomWidth: 1}}
                     placeholder="Qtde de Números"
-                    value={this.state.qtdNumeros}
+                    value={`${this.state.qtdNumeros}`}
                     onChangeText={this.changeQtdNumero}
                 />
+                <Button
+                    title="Gerar"
+                    onPress={this.gerarNumeros}
+                />
+                <Text>
+                {this.state.numeros.join(', ')}
+                </Text>
             </>
         )
     }
