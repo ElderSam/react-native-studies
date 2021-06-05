83e041f49595256c069e1c089fabd87e2b2ac082
ElderSam <eldersamuel98@gmail.com>
2021-06-05 14:52:26 -0300

    Button Component #01

--- curso-react-native/calculator/App.js
+++ curso-react-native/calculator/App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Calculator!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Button from './src/components/Button'
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
 });
