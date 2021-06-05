926d4a0dcfa9a0ff021456b917775b388ac2eb62
ElderSam <eldersamuel98@gmail.com>
2021-06-05 16:26:03 -0300

    Calculator Logic #02

--- curso-react-native/calculator/App.js
+++ curso-react-native/calculator/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
     if(n !== '.') {
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
-      values[this.state.curernt] = newValue
+      values[this.state.current] = newValue
       this.setState({ values })
     }
   }
@@ -40,7 +40,27 @@ export default class App extends Component {
   }
 
   setOperation = operation => {
+    if(this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] = 
+          eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch(e) {
+        values[0] = this.state.values[0]
+      }
 
+      values[1] = 0
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render() {
