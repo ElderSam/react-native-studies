a6ca961337cd8cb36daa85b020f613cc271517c3
ElderSam <eldersamuel98@gmail.com>
2021-06-13 15:53:31 -0300

    Drawer Navigation #02 (changing Drawer state)

---
+++ curso-react-native/navigation/src/views/TelaD.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { View, Button } from 'react-native'
+
+import TextoCentral from '../components/TextoCentral'
+
+export default props => {
+
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{paddingTop: 25, flexDirection: 'row', justifyContent: 'flex-end' }}>
+                <Button
+                    title='Abrir'
+                    onPress={() => {
+                        props.navigation.openDrawer()
+                        // setTimeout(() => {
+                        //     props.navigation.closeDrawer()
+                        //     setInterval(() => {
+                        //         props.navigation.toggleDrawer()
+                        //     }, 1000)
+                        // }, 3000)
+                    }}
+                />
+            </View>
+            <View style={{ flex: 1 }}>
+                <TextoCentral corFundo='#33fa72' corTexto='#000'>
+                    Tela D
+                </TextoCentral>
+            </View>
+        </View>
+    )
+}
