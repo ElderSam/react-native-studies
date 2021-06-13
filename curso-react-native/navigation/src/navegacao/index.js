5c4c5309c8f06b1cb7a75efaa9d7bce89f134f6a
ElderSam <eldersamuel98@gmail.com>
2021-06-13 15:35:11 -0300

    Drawer Navigation #01

--- curso-react-native/navigation/src/navegacao/index.js
+++ curso-react-native/navigation/src/navegacao/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SafeAreaView,  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import Tab from './Tab'
+import Drawer from './Drawer'
 
 export default props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
-            <Tab />
+            <Drawer />
+            {/* <Tab /> */}
             {/* <Stack /> */}
         </NavigationContainer>
     </SafeAreaView>
