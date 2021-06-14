ecb16760c40c7ad94f1e4f24368297c4338b392c
ElderSam <eldersamuel98@gmail.com>
2021-06-13 22:15:07 -0300

    Add Icons #01

--- curso-react-native/navigation/src/navegacao/index.js
+++ curso-react-native/navigation/src/navegacao/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { SafeAreaView,  } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import Drawer from './Drawer'
+import Tab from './Tab'
 
 export default props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
-            <Drawer />
-            {/* <Tab /> */}
+            {/* <Drawer /> */}
+            <Tab />
             {/* <Stack /> */}
         </NavigationContainer>
     </SafeAreaView>
